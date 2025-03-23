71f381d3930de68d1e635b8299f927dae2428cff
RavenHu001 <2419652744@qq.com>
2025-03-22 23:04:07 -0700

    modify code

--- Assets/MinMap/Script/MinMapCameraManager.cs
+++ Assets/MinMap/Script/MinMapCameraManager.cs
@@ -6,19 +6,54 @@ public class MinMapCameraManager : MonoBehaviour
     [SerializeField] Transform minMapCameraTransform; //小地图相机对象
     [SerializeField] Camera minMapCamera;
 
+    [SerializeField] private LayerMask[] floorLayers;  // 各层对应的图层（按顺序填充）
+    [[SerializeField] private float[] floorHeights;     // 各层的Y轴高度分界点（升序排列）
+
+
     public float cameraHeight = 20f; //小地图相机高度
     public float cameraSize = 20f; //小地图相机的视场大小
 
     private void Update()
     {
-        if (playerTransform == null) 
+        
+        UpdateCameraPosition();
+        //UpdateCameraLayer();
+    }
+
+    private void UpdateCameraPosition() 
+    {
+        if (playerTransform == null)
         {
             Debug.Log("No player found!");
+            return;
         }
         //随玩家移动实时更新小地图相机位置,只更新x和z，y固定为相机高度
         Vector3 newCameraPosition = new Vector3(playerTransform.position.x, cameraHeight, playerTransform.position.z);
         minMapCameraTransform.position = newCameraPosition;
         //防止相机旋转,锁定角度为x转90度
         minMapCameraTransform.rotation = Quaternion.Euler(90, 0, 0);
+    }
+    private void UpdateCameraLayer() 
+    {
+        if (minMapCamera == null) 
+        {
+            Debug.Log("No camera found!");
+            return;
+        }
+        // 检测玩家当前所在的层
+        float playerY = playerTransform.position.y;
+        int currentFloor = 0;
+
+        // 遍历高度分界点，确定当前层
+        for (int i = 0; i < floorHeights.Length - 1; i++)
+        {
+            if (playerY >= floorHeights[i] && playerY < floorHeights[i + 1])
+            {
+                currentFloor = i;
+                break;
+            }
+        }
+        // 更新摄像机的渲染层级
+        minMapCamera.cullingMask = floorLayers[currentFloor];
     }
 }

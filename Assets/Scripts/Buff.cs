6a2783c10124cf626735b7116537d464a412cb93
XuMing <144261944+prussia1891@users.noreply.github.com>
2025-03-23 20:44:52 -0700

    Update Buff.cs

--- Assets/Scripts/Buff.cs
+++ Assets/Scripts/Buff.cs
@@ -46,9 +46,11 @@ public class Buff : MonoBehaviour
                 playerController.ChangeSize(playerController.transform.localScale * 3f, 15f, "IncreaseSize"); // 变大并持续15秒
                 break;
             case BuffType.DecreaseSize:
+                Debug.Log("变小");
                 playerController.ChangeSize(playerController.transform.localScale * 0.5f, 15f, "DecreaseSize"); // 变小并持续15秒
                 break;
             case BuffType.EnableDash:
+                 Debug.Log("加速");
                 playerController.EnableDash(15f); // 启用冲刺并持续15秒
                 break;
             case BuffType.FreezeEnemy:

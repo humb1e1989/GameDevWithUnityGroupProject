b6a81bf244558a433e40a878cadf36b93e554d26
RavenHu001 <2419652744@qq.com>
2025-03-28 16:57:52 -0700

    use trigger to replace the collision enter

--- Assets/Scripts/GhostEffectControler.cs
+++ Assets/Scripts/GhostEffectControler.cs
@@ -8,37 +8,41 @@ public class GhostEffectControler : MonoBehaviour
     [Tooltip("暂停持续时间（秒）")]
     [SerializeField] private float pausedTime = 5f;
 
-    private bool isPaused;
+    [SerializeField] private bool isPaused=false;
 
     private void Start()
     {
-        isPaused = false;
+
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("碰撞检测到: " + collision.gameObject.name + ", 标签: " + collision.gameObject.tag);
-        HandleCollision(collision.gameObject);
+        Debug.Log(gameObject.name+ " 触发器检测到: " + other.gameObject.name + ", 标签: " + other.gameObject.tag);
+        HandleCollision(other.gameObject);
     }
     private void HandleCollision(GameObject otherObject) 
     {
         // 检查是否与玩家碰撞
-        if (otherObject.CompareTag(playerTag)&&(!isPaused))
+        if (otherObject.CompareTag(playerTag))
         {
-            Debug.Log("检测到碰撞玩家，执行暂停。");
+            Debug.Log(gameObject.name+" 检测到碰撞玩家，执行暂停。");
             PausedGhost();
         }
     }
     //暂停敌人动作
     private IEnumerator PausedGhost() 
-    { 
-        //暂停
-        isPaused=true;
-        GetComponent<Collider>().enabled=false;
-        //等待暂停时间
-        yield return new WaitForSeconds(pausedTime);
-        //重置
-        isPaused=false;
-        GetComponentInChildren<Collider>().enabled=true;
+    {
+        if (!isPaused)
+        {
+            //暂停
+            isPaused = true;
+            GetComponent<Collider>().enabled = false;
+            //等待暂停时间
+            yield return new WaitForSeconds(pausedTime);
+            //重置
+            isPaused = false;
+            GetComponentInChildren<Collider>().enabled = true;
+            Debug.Log(gameObject.name+" 结束暂停。");
+        }
     }
 }
